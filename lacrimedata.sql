4feeb1987ab7aeccd83ce12c4e18d5df50c2105a
SebasLee98 <sebastianhclee@gmail.com>
2025-11-22 21:40:25 -0800

    crime analysis

--- lacrimedata.sql
+++ lacrimedata.sql
@@ -1,1 +1,6 @@
-SELECT * FROM crime_data_from_2020_to_present WHERE `Vict Sex` = "M" ORDER BY AREA DESC
+SELECT * FROM crime_data_from_2020_to_present WHERE `Date Rptd` LIKE '%2021%'
+
+SELECT Area, count(`Crm Cd Desc`) FROM crime_data_from_2020_to_present WHERE `Crm Cd Desc` = 'ROBBERY' GROUP BY Area
+
+-- Locating a particular Theft suspects targeting females in Topanga Divison 
+SELECT * FROM crime_data_from_2020_to_present WHERE `Crm Cd Desc` LIKE '%THEFT PLAIN%' AND `Vict Sex` = 'F' AND `AREA NAME` = 'Topanga'
